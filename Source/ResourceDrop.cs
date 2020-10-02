68387b9af1f7013039feab72e2dc6645e05c233d
Nyinxie <72283863+Nyinxie@users.noreply.github.com>
2020-10-03 00:15:26 +0100

    Update ResourceDrop.cs

--- Source/ResourceDrop.cs
+++ Source/ResourceDrop.cs
@@ -10,6 +10,7 @@ namespace AvaliMod
         {
             List<Thing> thingList = new List<Thing>();
             thingList.Add(ThingMaker.MakeThing(AvaliDefs.AvaliNanoForge));
+         ///             (ThingMaker.MakeThing(AvaliDefs.AvaliNanoForge.minfiedDef)); //Suggest replacing above code with this, prevents the placement of the ThingDef with a MinifiedThing def, preventing destruction of property and other Things
             Map target = (Map)parms.target;
             IntVec3 intVec3 = DropCellFinder.TradeDropSpot(target);
             if (RimValiUtility.PawnOfRaceCount(Faction.OfPlayer, AvaliDefs.RimVali) >= 5)
